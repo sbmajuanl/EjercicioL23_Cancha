492955abaf89afc099502f101f98fac9e22118ad
Brilly Majuan <sbmajuanl@gmail.com>
2017-04-20 12:11:17 -0500

    terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,38 @@
 var content = document.getElementById("content");
 
-/* Creando elementos y añadiendo sus clases **/
-var cancha = document.createElement("div");
+var cancha = document.createElement("cancha");
 cancha.setAttribute("class","cancha");
 
-var arcoLeft = document.createElement("div");
-arcoLeft.setAttribute("class",'arcoLeft rectangule');
+var arcoLeft = document.createElement("arcoLeft");
+arcoLeft.setAttribute("class","arcoLeft rectangule");
 
-var arcoRight = document.createElement("div");
+var arcoRight = document.createElement("arcoRigth");
 arcoRight.setAttribute("class",'arcoRight rectangule');
 
-var circule = document.createElement("div");
-circule.setAttribute("class", "circule-center");
+var circule = document.createElement("circule");
+circule.setAttribute("class", "circule");
 
-var ball1= document.createElement("div");
-ball1.setAttribute("class", "balls ball1");
+var balon1= document.createElement("balon1");
+balon1.setAttribute("class", "balon balon1");
 
-var ball2= document.createElement("div");
-ball2.setAttribute("class", "balls ball2");
+var balon2= document.createElement("balon2");
+balon2.setAttribute("class", "balon balon2");
 
-var ball3= document.createElement("div");
-ball3.setAttribute("class", "balls ball3");
+var balon3= document.createElement("balon3");
+balon3.setAttribute("class", "balon balon3");
 
-var texto = document.createElement("p");
-texto.setAttribute("class","texto");
-texto.appendChild(document.createTextNode("Laboratoria 2017 created by Liliana Peña"));
+var info = document.createElement("info");
+info.setAttribute("class","info");
+info.appendChild(document.createTextNode("Laboratoria 2017 created by Schelsen Majuan"));
 
-/* Agrendo pelotas a los dos arcos */
-arcoLeft.appendChild(ball1);
-arcoRight.appendChild(ball3);
 
-/* Agregando pelota a la cancha */
-cancha.appendChild(ball2);
+arcoLeft.appendChild(balon1);
+arcoRight.appendChild(balon3);
 
-/* Agregando arcos a la cancha */
+cancha.appendChild(balon2);
 cancha.appendChild(arcoLeft);
 cancha.appendChild(arcoRight);
+cancha.appendChild(circule);
 
-/* Agregando circulo central y la cancha al contenido */
-content.appendChild(circule);
 content.appendChild(cancha);
-content.appendChild(texto);
+content.appendChild(info);
